bbb67cae2988e83ac87513f1dddf1f2ce6f2b971
Steve Howe <Stevehowe10@gmail.com>
2019-10-21 14:20:12 -0600

    initial push - front and back end working

--- public/assets/javascript/ajax.js
+++ public/assets/javascript/ajax.js
@@ -1,42 +1,40 @@
 function prices(){
-    // Bitcoin price
+        
     $.ajax({url: "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", success: function(response){
-     
-    var btcLast = response.last    
+        
+        var btcLast = response.last    
         console.log("BTC average: " + btcLast) 
-       
+   
     }});
-
+    
     $.ajax({url: "https://api.gemini.com/v1/pubticker/btcusd", success: function(response){
-       
-    var btcLast = response.last    
+   
+        var btcLast = response.last    
         console.log("BTC average: " + btcLast) 
-       
+        
     }});
-
-    $.ajax({url: "https://api.binance.us/api/v3/ticker/price?symbol=BTCUSDT", success: function(response){
-         console.log(response)
+    
+    $.ajax({url: "http://cors-anywhere.herokuapp.com/https://api.binance.us/api/v3/ticker/price?symbol=BTCUSDT", success: function(response){
+        console.log(response)
         // var btcLast = response.last    
-        //     console.log("BTC average: " + btcLast) 
-           
-        }});
-
+        //     console.log("BTC average: " + btcLast)  S          
+    }});
+    
     $.ajax({url: "https://api.coinbase.com/v2/prices/:USD_BTC/buy", success: function(response){
         console.log(response)
-    // var btcLast = response.data.amount    
-    //     console.log("BTC average: " + btcLast) 
+        // var btcLast = response.data.amount    
+        //     console.log("BTC average: " + btcLast) 
         
-        }});
-
+    }});
+    
     $.ajax({url: "https://api.kraken.com/0/public/Ticker?pair=XBTUSD", success: function(response){
-       
-    var btcLast = response.result.XXBTZUSD.b[0]    
+        
+        var btcLast = response.result.XXBTZUSD.b[0]    
         console.log("BTC average: " + btcLast) 
         
-        }});
-
-
+    }});
+    
+    
 }
 
 prices();
-
